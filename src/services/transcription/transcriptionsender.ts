170f6affec9ef7b18d166acf639e6076d04522e0
Yey007 <yey007tech@gmail.com>
2020-11-07 21:15:31 -0500

    Fixed bug with curse words

--- src/services/transcription/transcriptionsender.ts
+++ src/services/transcription/transcriptionsender.ts
@@ -33,7 +33,7 @@ export class TranscriptionSender {
 
         //Replace hesitation markers
         formatted = formatted.replace(/%HESITATION/g, "...")
-        formatted = formatted.replace("*", "\\*")
+        formatted = formatted.replaceAll("*", "\\*")
 
         let embed = this.maker.makeInfo()
         embed.title = ""
