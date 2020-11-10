aec968dc6f5deb249c12064f59dabcddb13ecbc8
Yey007 <yey007tech@gmail.com>
2020-11-09 21:03:05 -0500

    Added migration support

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,15 @@
-import migrate from "migrate"
+import migrate from "db-migrate"
 import { container } from "./inversify.config"
 import { Bot } from "./bot"
 import { TYPES } from "./types"
 
 const bot: Bot = container.get<Bot>(TYPES.Bot)
+const dbm = migrate.getInstance(true)
 
-migrate.load({stateStore: '.migrate'}, function (err, set) {
-    if (err) {
-        throw err
-    }
-    set.up(function (err) {
-        if (err) {
-            throw err
-        }
-        console.log('Ran migrations successfully.')
-        bot.start().then(() => {
-            console.log('Logged in!')
-        }).catch((error) => {
-            console.log('Oh no!!! ', error)
-        });
-    })
+dbm.up().then(() => {
+    bot.start().then(() => {
+        console.log('Logged in!')
+    }).catch((error) => {
+        console.log('Oh no!!! ', error)
+    });
 })
