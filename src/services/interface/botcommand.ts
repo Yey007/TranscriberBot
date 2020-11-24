9d0ed0f209c1fef6ed12457d128371fb7e99c15b
Yey007 <yey007tech@gmail.com>
2020-11-24 18:16:42 -0500

    Fixed transcript channels not being set

--- src/services/interface/botcommand.ts
+++ src/services/interface/botcommand.ts
@@ -4,7 +4,7 @@ import { CommandArgs } from "./commandargs";
 
 @injectable()
 export abstract class BotCommand {
-    public async abstract execute(message: Message, args: string[]): Promise<void>
+    public abstract execute(message: Message, args: string[]): Promise<void>
     public abstract get help(): string
     public abstract get args(): CommandArgs[]
 }
